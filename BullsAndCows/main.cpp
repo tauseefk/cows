4db4dd6680c81199aa934dcc3c724f2e5001e6c2
tauseefk <tauseef25@gmail.com>
2018-10-14 11:23:36 -0700

    Initial Commit

---
+++ BullsAndCows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullsAndCows
+//
+//  Created by Md Tauseef on 10/14/18.
+//  Copyright © 2018 Md Tauseef. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
